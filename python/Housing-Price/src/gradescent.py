38fe32645376d75cb16baadb68d0bb0bbb04dabc
[pravy24] <[praveen.vijayan@gmail.com]>
2017-03-04 23:32:19 +0530

    linear regression draft1

--- python/Housing-Price/src/gradescent.py
+++ python/Housing-Price/src/gradescent.py
@@ -53,6 +53,17 @@ def gradientDescent(X, y, theta, alpha, lamda, iterations):
 
 
 
+def normalize_feature(X):
+    
+        meanX = X.mean(axis=0)
+        meanX[0] = 0.0
+        stdX = X.std(axis=0)
+        stdX[0] = 1.0
+        
+        return((X - meanX)/stdX)
+    
+        
+
 def predict(X, theta):
     
     return (X.dot(theta))
@@ -63,8 +74,8 @@ def predict(X, theta):
 """ The MAIN """
 if __name__ == "__main__":
 
-    alpha = .01
-    lamda = 1000
+    alpha = .1
+    lamda = 1
     iterations = 1500
         
     # read_csv : load the csv file into a DataFrame,
@@ -73,7 +84,7 @@ if __name__ == "__main__":
     # keep_default_na : if False, then override the default NaN values.'''
     # fillna(0) : replaces the default NA values with 0,
     hpdata = pd.read_csv(\
-                "D:\IC019134\Learnings\Data Science\workarea\python\Housing-Price\data\hp_train.csv", 
+                "..\data\hp_train.csv", 
                 usecols=['LotFrontage', 'LotArea', 'YearBuilt',
                          'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2','BsmtUnfSF', 
                          'TotalBsmtSF', '1stFlrSF','2ndFlrSF', 'LowQualFinSF', 
@@ -85,33 +96,49 @@ if __name__ == "__main__":
                 na_values={'LotFrontage':['NA', 'na']},
                 keep_default_na=False).fillna(0)
 
+#    hpdata = pd.read_csv(\
+#                "..\data\hp_train.csv", 
+#                usecols=['LotFrontage', 'LotArea', 'SalePrice'],
+#                na_values={'LotFrontage':['NA', 'na'], 'LotArea':['NA', 'na']},
+#                keep_default_na=False).fillna(0)
+
     m = len(hpdata)
     n = len(hpdata.columns)-1 #exclude the predictor
     X = hpdata.iloc[0:m, 0:n] # DataFrame.iloc = index locator
     y = hpdata.iloc[0:m, n:n+1]
     y.columns = [0]
     
-    """numpy.ones creates an array of 1.0"""
-    """numpy.concatenate merges two arrays"""
-    """axis=1 for concatenate merges the arrays vertically"""
-    X = X.rename_axis({'LotFrontage':1, 'LotArea':2, 'YearBuilt':3, 'MasVnrArea':4, 
-                   'BsmtFinSF1':5, 'BsmtFinSF2':6,'BsmtUnfSF':7, 'TotalBsmtSF':8, 
-                   '1stFlrSF':9,'2ndFlrSF':10, 'LowQualFinSF':11, 'GrLivArea':12,
-                   'BsmtFullBath':13, 'BsmtHalfBath':14, 'FullBath':15, 'HalfBath':16, 
-                   'BedroomAbvGr':17, 'KitchenAbvGr':18, 'TotRmsAbvGrd':19, 'Fireplaces':20, 
-                   'GarageCars':21, 'GarageArea':22,'WoodDeckSF':23, 'OpenPorchSF':24, 
-                   'EnclosedPorch':25,'3SsnPorch':26, 'ScreenPorch':27, 'PoolArea':28,
-                   'MiscVal':29, 'MoSold':30, 'YrSold':31}, axis='columns')
+#    """numpy.ones creates an array of 1.0"""
+#    """numpy.concatenate merges two arrays"""
+#    """axis=1 for concatenate merges the arrays vertically"""
+#    X = X.rename_axis({'LotFrontage':1, 'LotArea':2, 'YearBuilt':3, 'MasVnrArea':4, 
+#                   'BsmtFinSF1':5, 'BsmtFinSF2':6,'BsmtUnfSF':7, 'TotalBsmtSF':8, 
+#                   '1stFlrSF':9,'2ndFlrSF':10, 'LowQualFinSF':11, 'GrLivArea':12,
+#                   'BsmtFullBath':13, 'BsmtHalfBath':14, 'FullBath':15, 'HalfBath':16, 
+#                   'BedroomAbvGr':17, 'KitchenAbvGr':18, 'TotRmsAbvGrd':19, 'Fireplaces':20, 
+#                   'GarageCars':21, 'GarageArea':22,'WoodDeckSF':23, 'OpenPorchSF':24, 
+#                   'EnclosedPorch':25,'3SsnPorch':26, 'ScreenPorch':27, 'PoolArea':28,
+#                   'MiscVal':29, 'MoSold':30, 'YrSold':31}, axis='columns')
     
-    X = pd.concat([pd.DataFrame(npy.zeros((m,1))), X], axis=1)
+    X = pd.concat([pd.DataFrame(npy.ones((m,1)), columns=['Intercept']), X], axis=1)
     n = n+1 #since an intercept column is added
     
-    theta = pd.DataFrame(npy.ones((n, 1)), index=range(0, 32))
+    X_norm = normalize_feature(X)
     
-    cost = computeCost (X, y, theta, lamda)
+    theta = pd.DataFrame(npy.zeros((n, 1)), index=['Intercept', 'LotFrontage', 'LotArea', 'YearBuilt',
+                         'MasVnrArea', 'BsmtFinSF1', 'BsmtFinSF2','BsmtUnfSF', 
+                         'TotalBsmtSF', '1stFlrSF','2ndFlrSF', 'LowQualFinSF', 
+                         'GrLivArea','BsmtFullBath', 'BsmtHalfBath', 'FullBath',
+                         'HalfBath', 'BedroomAbvGr', 'KitchenAbvGr', 'TotRmsAbvGrd',
+                         'Fireplaces', 'GarageCars', 'GarageArea','WoodDeckSF', 
+                         'OpenPorchSF', 'EnclosedPorch','3SsnPorch', 'ScreenPorch', 
+                         'PoolArea','MiscVal', 'MoSold', 'YrSold'])
+    
+    cost = computeCost (X_norm, y, theta, lamda)
 
-    (theta, cost_hist) = gradientDescent(X, y, theta, alpha, lamda, iterations)
+    (theta, cost_hist) = gradientDescent(X_norm, y, theta, alpha, lamda, iterations)
         
-    p = predict(npy.array([1, 3.5]), theta)
+    #p = predict(npy.array([1, 3.5]), theta)
+    print(theta)
     
-    print(p)
+    #print(p)
